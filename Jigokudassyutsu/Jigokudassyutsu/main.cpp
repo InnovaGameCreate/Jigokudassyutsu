ae75cc89048ae65221d797fee51c092338089545
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-07-09 11:37:11 +0900

    test

--- Jigokudassyutsu/Jigokudassyutsu/main.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/main.cpp
@@ -6,5 +6,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	DrawPixel(320, 240, GetColor(255, 255, 255));// 点を打つ
 	WaitKey();// キー入力待ち
 	DxLib_End();//ＤＸライブラリ終了処理
+
 	return 0;
 }
