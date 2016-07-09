a4659c59496ae295d17fc9e837c09baae01eafbd
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-07-09 11:25:35 +0900

    test

--- Jigokudassyutsu/Jigokudassyutsu/main.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/main.cpp
@@ -1,1 +1,10 @@
-﻿
+﻿#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	ChangeWindowMode(TRUE);//ウィンドウモード
+	DxLib_Init();
+	DrawPixel(320, 240, GetColor(255, 255, 255));// 点を打つ
+	WaitKey();// キー入力待ち
+	DxLib_End();//ＤＸライブラリ終了処理
+	return 0;
+}
