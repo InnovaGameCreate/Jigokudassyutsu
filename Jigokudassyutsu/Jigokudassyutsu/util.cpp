563abd184af22d3217edae958ef1bdef61b413ff
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-07-23 19:36:27 +0900

    utilの不要になったエラー出力関数を削除

--- Jigokudassyutsu/Jigokudassyutsu/util.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/util.cpp
@@ -2,12 +2,6 @@
 
 namespace util {
 	//ƒGƒ‰[o—Í
-	void ErrorOutPut(const char file[], const char func[], int line,
-		const char message[]) {
-		fprintf(stderr, "<file:%s func:%s line:%d>\n", file, func, line);
-		fprintf(stderr, "Error:%s\n\n", message);
-	}
-
 	void ErrorOutPut(const char file[], const char func[], int line, std::string message) {
 		fprintf(stderr, "<file:%s func:%s line:%d>\n", file, func, line);
 		fprintf(stderr, "Error:%s\n\n", message.c_str());
--- Jigokudassyutsu/Jigokudassyutsu/util.h
+++ Jigokudassyutsu/Jigokudassyutsu/util.h
@@ -8,10 +8,6 @@
 namespace util {
 	//エラー出力
 	//util::ErrorOutPut(__FILE__, __func__, __LINE__, "エラーメッセージ");の形で呼び出してください
-	void ErrorOutPut(const char file[], const char func[], int line, const char message[]);
-
-	//エラー出力(c++版)
-	//util::ErrorOutPut(__FILE__, __func__, __LINE__, "エラーメッセージ");の形で呼び出してください
 	void ErrorOutPut(const char file[], const char func[], int line, std::string message);
 }
 
