e50c95340868c3ae524667a3d17af5cc4a5ed9eb
Owner <Owner@OWNER-PC>
2016-07-29 15:22:20 +0900

    関数の位置を修正

--- Jigokudassyutsu/Jigokudassyutsu/player.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/player.cpp
@@ -10,19 +10,17 @@ Player::~Player() {
 
 //初期化
 void Player::Initialize() {
+	h_cursor_ = LoadCursorFromFile("Mario.cur");
 }
 
 //更新
 void Player::Update() {
+	SetCursor(h_cursor_);
 }
 
 //描画
 void Player::Draw()const {
 	DrawString(0, 80, "プレイヤー", GetColor(255, 255, 255));
-	
-	HCURSOR hCursor;
-	hCursor = LoadCursorFromFile("Mario.cur");
-	SetCursor(hCursor);
 }
 
 //終了処理
