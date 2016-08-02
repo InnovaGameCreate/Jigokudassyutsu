6bffe0517569070b69819fcbe5f250e3c1f734bf
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-08-02 12:41:08 +0900

    collisionクラスを型枠だけ作成,opencvを使えるようにする途中

---
+++ Jigokudassyutsu/Jigokudassyutsu/collision.cpp
@@ -0,0 +1,20 @@
+#include "collision.h"
+
+CollisionRoad::CollisionRoad(int stage_num) :stage_num_(stage_num){
+}
+
+CollisionRoad::~CollisionRoad() {
+}
+
+void CollisionRoad::Initialize() {
+}
+
+void CollisionRoad::Finalize() {
+}
+
+bool CollisionRoad::Update(float x, float y, float radius) {
+	return false;
+}
+
+void CollisionRoad::Draw()const {
+}
---
+++ Jigokudassyutsu/Jigokudassyutsu/collision.h
@@ -0,0 +1,19 @@
+#ifndef JIGOKUDASSYUTSU_COLLISION_COLLISION_H_
+#define JIGOKUDASSYUTSU_COLLISION_COLLISION_H_
+
+#include "DxLib.h"
+#include "task.h"
+
+class CollisionRoad {
+private:
+	const int stage_num_;
+public:
+	CollisionRoad(int stage_num);
+	~CollisionRoad();
+	void Initialize();
+	void Finalize();
+	bool Update(float x, float y, float radius);
+	void Draw()const;
+};
+
+#endif
