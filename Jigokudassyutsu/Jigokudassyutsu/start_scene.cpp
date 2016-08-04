414f40cdb6aafbaaf09aec088cc25021a62bbe70
Ito-Shin <c611601374@tokushima-u.ac.jp>
2016-08-05 06:25:14 +0900

    画像の貼り付け、ゲームを終了するキーの追加

--- Jigokudassyutsu/Jigokudassyutsu/start_scene.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/start_scene.cpp
@@ -1,5 +1,7 @@
 #include "start_scene.h"
 
+static int handle01;
+
 //コンストラクタ
 StartScene::StartScene(ISceneChanger* changer) : BaseScene(changer) {
 }
@@ -14,15 +16,23 @@ void StartScene::Initialize() {
 
 //更新
 void StartScene::Update() {
-	if (input::CheckStateKey(KEY_INPUT_ESCAPE) == 1) { //Escキーが押されていたら
+	if (input::CheckStateKey(KEY_INPUT_RETURN) == 1) { //Enterキーが押されていたら
 		scene_changer_->ChangeScene(kSceneStageSelect);//シーンをメニューに変更
+	}
+
+	if (input::CheckStateKey(KEY_INPUT_ESCAPE) == 1) { //Escキーが押されていたら
+		DxLib_End();                                   //ゲームを終了
 	}
 }
 
 //描画
 void StartScene::Draw()const {
+	handle01 = LoadGraph("enemy.png"); //画像の読み込み
+	DrawGraph(0, 0, handle01, TRUE);  // 読みこんだグラフィックを画面左上に描画
+
 	DrawString(0, 0, "スタート画面です。", GetColor(255, 255, 255));
-	DrawString(0, 20, "Escキーを押すとステージ選択画面に進みます。", GetColor(255, 255, 255));
+	DrawString(0, 20, "Enterキーを押すとステージ選択画面に進みます。", GetColor(255, 255, 255));
+	DrawString(0, 40, "Escキーを押すとゲームを終了します。", GetColor(255, 255, 255));
 }
 
 //終了処理
