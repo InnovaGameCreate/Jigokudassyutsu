01ac3d0439b1cf419b02b35385fc64b84960606e
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-08-10 22:05:57 +0900

    とりあえず道の当たり判定完成

--- Jigokudassyutsu/Jigokudassyutsu/collision.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/collision.cpp
@@ -12,7 +12,7 @@ CollisionRoad::~CollisionRoad() {
 
 void CollisionRoad::Initialize() {
 	//当たり判定用の画像読み込み
-	col_map_ = cv::imread("img/collision/test_road2.png", cv::IMREAD_UNCHANGED);
+	col_map_ = cv::imread("img/collision/test_road.png", cv::IMREAD_UNCHANGED);
 	//エラー処理
 	if (col_map_.data == NULL) {
 		util::ErrorOutPut(__FILE__, __func__, __LINE__, "画像の読み込みに失敗");
@@ -20,41 +20,33 @@ void CollisionRoad::Initialize() {
 	}
 	if (col_map_.channels() != 4)
 		util::ErrorOutPut(__FILE__, __func__, __LINE__, "アルファチャンネル付きの画像ではありません");
-	if (col_map_.rows<0 || col_map_.rows>kMapHeight || col_map_.cols<0 || col_map_.cols>kMapWidth) {
+	if (col_map_.rows != kMapHeight || col_map_.cols != kMapWidth) {
 		util::ErrorOutPut(__FILE__, __func__, __LINE__, "画像サイズが不正です");
-		col_map_width_ = 0;
-		col_map_height_ = 0;
+		exit(1);
 	}
-	else {
-		col_map_width_ = col_map_.cols;
-		col_map_height_ = col_map_.rows;
-	}
-	//比率計算
-	width_ratio_ = (double)col_map_width_ / kMapWidth;
-	height_ratio_ = (double)col_map_height_ / kMapHeight;
-
-	//以下後で削除
-	//テスト画像
-	test_handle_ = LoadGraph("img/collision/test_road.png");
 }
 
 void CollisionRoad::Finalize() {
 }
 
 bool CollisionRoad::Update(float x, float y, float radius) {
-	int cx = x*width_ratio_;
-	int cy = y*height_ratio_;
-	if (IsColMapAlpha(cx, cy))
+	//中心を調査
+	if (IsColMapAlpha(x, y) == true)
 		return true;
+	//周りの8点を調査
+	for (double t = 0; t <= DX_PI*2.0; t += DX_PI / 4.0) {
+		if (IsColMapAlpha(radius*sin(t) + x, radius*cos(t)) + y == true) {
+			return true;
+		}
+	}
 	return false;
 }
 
 void CollisionRoad::Draw()const {
-	DrawExtendGraph(0, 0, kMapWidth, kMapHeight, test_handle_, TRUE);
 }
 
 inline bool CollisionRoad::IsColMapAlpha(int x, int y) {
-	if (x<0 || x>col_map_width_ || y<0 || y>col_map_height_) {
+	if (x<0 || x>kMapWidth || y<0 || y>kMapHeight) {
 		return false;
 	}
 	if (col_map_.data[y * col_map_.step + x * col_map_.elemSize() + 3] == 0)
--- Jigokudassyutsu/Jigokudassyutsu/collision.h
+++ Jigokudassyutsu/Jigokudassyutsu/collision.h
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <opencv2/highgui/highgui.hpp>
+#include <math.h>
 
 class CollisionRoad {
 private:
@@ -14,11 +15,6 @@ private:
 	const int kMapWidth;	//マップの横ピクセル数
 	const int kMapHeight;	//マップの縦ピクセル数
 	cv::Mat col_map_;		//当たり判定用の画像のデータ
-	int col_map_width_;		//当たり判定用の画像の横ピクセル数
-	int col_map_height_;	//当たり判定用の画像の縦ピクセル数
-	double width_ratio_;	//当たり判定用の画像とマップの横ピクセル比
-	double height_ratio_;	//当たり判定用の画像とマップの縦ピクセル比
-	int test_handle_;
 
 	inline bool IsColMapAlpha(int x, int y);//x,yの位置が当たり判定画像で透過部分ならtrueを返す
 
