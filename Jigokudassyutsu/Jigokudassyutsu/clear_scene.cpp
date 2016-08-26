076306b7634dd17a7661e5e151bee45e99179a6e
Owner <Owner@OWNER-PC>
2016-08-26 18:00:13 +0900

    クリア画像の描画

--- Jigokudassyutsu/Jigokudassyutsu/clear_scene.cpp
+++ Jigokudassyutsu/Jigokudassyutsu/clear_scene.cpp
@@ -10,6 +10,11 @@ ClearScene::~ClearScene() {
 
 //初期化
 void ClearScene::Initialize() {
+	clear_image_ = LoadGraph("img/back/clear.jpg");   //画像の読み込み
+	if (clear_image_ == -1)
+		util::ErrorOutPut(__FILE__, __func__, __LINE__, "読み込めませんでした。");   //エラー処理
+
+	font_handle_ = CreateFontToHandle("游明朝 Demibold", 50, 2);
 }
 
 //更新
@@ -21,10 +26,15 @@ void ClearScene::Update() {
 
 //描画
 void ClearScene::Draw()const {
+	DrawRotaGraph(320, 240, 1.0, 0, clear_image_, TRUE);   //画像の描画
+
 	DrawString(0, 0, "クリア画面です。", GetColor(255, 255, 255));
 	DrawString(0, 20, "Escキーを押すとスタート画面に戻ります。", GetColor(255, 255, 255));
+	DrawStringToHandle(90, 150, "ゲームクリア！", GetColor(0, 0, 0), font_handle_);   //文字の描画
 }
 
 //終了処理
 void ClearScene::Finalize() {
+	DeleteGraph(clear_image_);
+	DeleteFontToHandle(font_handle_);
 }
